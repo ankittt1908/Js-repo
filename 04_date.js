442a3f6324c7ad4dfa34d82f311d68ac1737c488
ankittt1908 <singhankit7407@gmail.com>
2024-08-27 15:20:07 +0530

    add me

--- 04_date.js
+++ 04_date.js
@@ -1,3 +1,12 @@
 let myDate  = new Date()
-console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(typeof(myDate));
+
+let myCreatedDate = new Date(2024, 8, 3, 23, 50, 35)
+console.log(myCreatedDate.toLocaleString());
 
